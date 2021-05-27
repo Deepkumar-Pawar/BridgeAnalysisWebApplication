bf135bdf83f77b2093ec25867e3a3adb0fc9c0fa
Deepkumar Pawar <pawardeepkumar@gmail.com>
2021-05-27 15:39:15 +0100

    started conencting to database work

--- Pages/AnalyzeBridge.cshtml.cs
+++ Pages/AnalyzeBridge.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MySql.Data.MySqlClient;
 
 namespace BridgeAnalysisWebApplication.Pages
 {
@@ -20,6 +21,11 @@ namespace BridgeAnalysisWebApplication.Pages
             string beamWeight = Request.Form["beamWeight"];
             string beamMaterial = Request.Form["beamMaterial"];
             string loadType = Request.Form["loadType"];
+
+            string connectionString = "Server=localhost;Database=bridgeanalysisdb;Uid=root;Pwd=;";
+
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
         }
     }
 }
